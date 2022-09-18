87c843a41a5a8d2e4aab2879bde1d8a5320570e1
WizarDRW <wnokroot@gmail.com>
2022-09-18 14:37:30 +0300

    Chapter model created.

---
+++ src/models/chapter.model.ts
@@ -0,0 +1,14 @@
+export interface Chapter {
+    name: String;
+    impressions: any[];
+    categories: any[];
+    description: String;
+    descriptions: any[];
+    status: Boolean;
+    image_path: String;
+    short_description: String;
+    create_date: Date;
+    showcases: any[];
+    tags: any[];
+    user_id: String;
+}
