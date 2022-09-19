ecd27f8864047acd66b07da2d23f457c3fa96d97
WizarDRW <wnokroot@gmail.com>
2022-09-19 19:38:01 +0300

    Forum model created.

---
+++ src/models/forum.model.ts
@@ -0,0 +1,14 @@
+export interface Forum {
+    name: String;
+    impressions: any[];
+    categories: any[];
+    description: String;
+    status: Boolean;
+    create_date: Date;
+    update_date: Date;
+    comments: any[];
+    short_description: String;
+    tags: any[];
+    connects: any[];
+    user_id: String;
+}
