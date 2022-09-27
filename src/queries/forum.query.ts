676073ad74362e0264ba852dc4245e836a9915a7
WizarDRW <wnokroot@gmail.com>
2022-09-27 20:22:09 +0300

    Forum query created.

---
+++ src/queries/forum.query.ts
@@ -0,0 +1,15 @@
+export const ForumQuery = `{
+    _id,
+    name,
+    impressions,
+    categories,
+    description,
+    status,
+    create_date,
+    update_date,
+    comments,
+    short_description,
+    tags,
+    connects,
+    user_id
+}`
