e45d59bc73fbf339038b5734a82047c2ba48d249
WizarDRW <wnokroot@gmail.com>
2022-09-30 01:44:46 +0300

    News query created.

---
+++ src/queries/news.query.ts
@@ -0,0 +1,16 @@
+export const NewsQuery = `{
+    _id,
+    name,
+    impressions,
+    categories,
+    description,
+    descriptions,
+    status,
+    image_path,
+    short_description,
+    create_date,
+    showcases,
+    tags,
+    connects,
+    user_id
+}`
