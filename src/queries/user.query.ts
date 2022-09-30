141f8c7780008fb6805ccac56033bd1af9c263db
WizarDRW <wnokroot@gmail.com>
2022-09-30 17:57:08 +0300

    User query created.

---
+++ src/queries/user.query.ts
@@ -0,0 +1,17 @@
+export const UserQuery = `{
+    _id,
+    first_name,
+    last_name,
+    username,
+    email,
+    password,
+    image_path,
+    description,
+    role,
+    title,
+    status,
+    create_date,
+    update_date,
+    connections,
+    type
+}`
